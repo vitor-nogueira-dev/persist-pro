4a56b2b02c0fc9377694d3e027e72d398da7d25d
Vitor Nogueira <vslnogueira96@gmail.com>
2023-11-25 18:32:13 -0300

    feat: Add new exports for local storage operations

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
-/**
- * Use this function to create a greeting string with the given name
- * @param name The name to greet
- */
-export const makeHello = (name?: string): string => `Hello ${name || 'world'}`;
+
+export { useLocalStorage } from './components/useLocalStorage';
+
+export {
+  setLocalStorage,
+  getLocalStorage,
+  removeItemLocalStorage,
+  clearLocalStorage
+}
+  from './storage';
+
